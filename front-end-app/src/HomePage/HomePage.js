c11ec13833f58fce9e8b4b4ef5cef3ef1537b25d
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2022-03-29 15:01:22 -0700

    react and everything

---
+++ front-end-app/src/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {createStore} from "redux";
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="">
+
+            </div>
+
+        </>
+    )
+}
